61e74dae8602aab58548847e6fd50ab0bc6c1193
bondydaa <bondydaa@gmail.com>
2013-05-29 20:16:18 -0500

    added jquery to rehide form inputs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$('.events input, label, a').not('#loc1, .loc1').hide()
-	$('a .add').click(function(){
+	$('.loc1, .add').click(function(){
 		$('.events input, label, a').not('#loc3, .loc3, #loc4, .loc4').show()
 	})
-
+	$('.loc2, .remove').click(function(){
+		$('.events input, label, a').not('#loc1, .loc1').hide()
+	})
 });
