87df33783a702b7d2fa8c5887b173601b9e37c6d
bondydaa <bondydaa@gmail.com>
2013-05-29 20:03:13 -0500

    added jquery ability to show hidden form inputs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	$('.events input, label').not('#loc1, .loc1').hide()
-
-
+	$('.events input, label, a').not('#loc1, .loc1').hide()
+	$('a .add').click(function(){
+		$('.events input, label, a').not('#loc3, .loc3, #loc4, .loc4').show()
+	})
 
 });
