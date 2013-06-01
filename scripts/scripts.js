f2fcd8dfa6ca039a6d7bd86b7232d981bc4a5b4b
bondydaa <bondydaa@gmail.com>
2013-06-01 10:35:19 -0500

    adding some changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 	$('.events input, label, a').not('#loc1, .loc1').hide()
-	$('.loc1, .add').click(function(){
+	$('.loc1').find('.add').click(function(){
 		$('.events input, label, a').not('#loc3, .loc3, #loc4, .loc4').show()
 	})
-	$('.loc2, .remove').click(function(){
+	$('.loc2').find('.remove').click(function(){
 		$('.events input, label, a').not('#loc1, .loc1').hide()
+	})
+	$('.loc2').find('.add').click(function(){
+		$('.events inputs, label, a').not('#loc4, .loc4').show()
 	})
 });
